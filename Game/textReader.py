7dee9412b28b10b2621eea1418a8c0798080e41f
Jaysownay <35205243+Jaysownay@users.noreply.github.com>
2018-09-20 14:11:39 -0700

    Delete textReader.py

--- Game/textReader.py
+++
@@ -1,8 +0,0 @@
-file = open("Gradient Walk.txt","w") 
- 
-file.write("Hello World\n") 
-file.write("This is our new text file\n") 
-file.write("and this is another line.\n") 
-file.write("Why? Because we can.\n") 
- 
-file.close() 
